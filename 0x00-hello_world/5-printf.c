6964005cdbef60679b1e73b7c122bd76acd3bbc7
rachelokello <okelloawuor@gmail.com>
2022-03-10 13:21:23 -0800

    stdio.h

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - Entry point of the program
  *
  * Return: 0 if the program is run successfully else return 1
- * /
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
