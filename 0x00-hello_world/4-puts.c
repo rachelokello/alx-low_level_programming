ebbad869b7c42d0b014eac94cffb23721cafd398
rachelokello <okelloawuor@gmail.com>
2022-03-14 06:37:32 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - Entry point of the program
  *
  * Return: 0 if the program is run successfully else return 1
- * /
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
