0ff07341c7f799ffd81d505462823382f9ade9b9
rachelokello <okelloawuor@gmail.com>
2022-03-15 12:15:17 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "main.h"
 
 /**
- * main - prints _putchar followed by a newline
+ * main - Entry point
  *
- * Return: if runs successfully return 0
+ * Return: Always 0
  */
 int main(void)
 {
-	char str[] = {95, 112, 117, 116, 99, 104 97, 114};
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		putchar(str[i]);
-	}
-	putchar('\n');
 	return (0);
 }
