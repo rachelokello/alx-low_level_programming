6f2244c4934fbc1aeb4eae0690f219cca79c65d8
rachelokello <okelloawuor@gmail.com>
2022-03-24 05:01:01 -0700

    more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,13 +15,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++);
 
-	while (dest[index++])
-		dest_len++;
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
 
-	for (index = 0; src[index] && index < n; index++
-			dest[dest_len++] = src[index];
+/*should end with a end of string char*/
+dest[dest_len + i} = '\0';
 
-			return (dest);
+return (dest);
+}
 			}
