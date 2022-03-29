d4c7dd52a54ab4df2a32f15e0c36aad3bf424691
rachelokello <okelloawuor@gmail.com>
2022-03-29 02:42:48 -0700

    even more pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-int _putchar.c
+int _putchar (char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
