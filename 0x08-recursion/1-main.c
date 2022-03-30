f3203740e69977d2683a93ca095b4bd991926cdb
rachelokello <okelloawuor@gmail.com>
2022-03-30 12:11:36 -0700

    recursion

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_print_rev_recursion("\ncolton walker");
+	return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,7 @@
+this readme file contains
+4-pow_recursion.c
+5-sqrt_recursion.c
+6-is_prime_number.c
+3-factorial.c
+2-strlen_recusrion.c
+1-print_rev_recursion.c
