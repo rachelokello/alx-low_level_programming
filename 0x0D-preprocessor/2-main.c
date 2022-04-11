26d78de9302513ac2acaa73abb8d822dd797a3b8
rachelokello <okelloawuor@gmail.com>
2022-04-11 11:48:16 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,19 +1,12 @@
-/*
- * File: 2-main.c
- * Auth: Rachel Okello
- */
-
 #include <stdio.h>
 
 /**
- * main - prints the name of the file of the program
- *            was compiled from, followed by a new line.
+ * main - prints the name of the file
  *
- *Return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	printf("%s\n", _FILE_);
-
 	return (0);
 }
