c6d1f66ba474f663cc76dee9821882e740effab9
rachelokello <okelloawuor@gmail.com>
2022-04-11 10:32:42 -0700

    preprocessors

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * file: 0-object_like_macro.h
+ * description: header file that defines a macro names SIZE as an abreviation for the token 1024
+ *
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+tthis readme file contains 0-object_like_macro.h and other files inside 0x0D-preprocessor
