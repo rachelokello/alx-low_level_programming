ef397bd50a9ccf56980bd83d005129617b26ef39
rachelokello <okelloawuor@gmail.com>
2022-04-13 07:13:19 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
- *print_name - function that prints a name
- *@name : name
- *@f: function
+ * print_name - prints a name 
+ * @name: the name to be printed
+ * @f: pointer to function
+ *
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - ...
+ * @array: ...
+ * @size: ...
+ * @action: ...
  *
- * Return: Nothing
+ * Return: ...
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
-/**
- * @size: size of te array
- * @action: pointer to the function in use
- *
- * Return: nothing
- */
+{
+	unsigned int i = 0;
 
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
