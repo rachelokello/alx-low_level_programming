117c54684ace9553d5583c7c4c00aaacc345a7e2
rachelokello <okelloawuor@gmail.com>
2022-04-14 11:32:18 -0700

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,12 +7,15 @@
 void print_all(const char *const format, ...)
 {va_list valist;
 	unsigned int i = 0, j, c = 0;
+
 	char *str;
+
 	const char t_arg[] = "cifs";
 	va_start(valist, format);
 	while (format && format[i])
 	{
 		j = 0;
+
 		while (t_arg[j])
 		{
 			if (format[i] == t_arg[j} && c)
