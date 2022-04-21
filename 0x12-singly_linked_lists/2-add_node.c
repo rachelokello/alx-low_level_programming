f3ffeb3648ed0ec494f0615bfa8a3c12702f52db
rachelokello <okelloawuor@gmail.com>
2022-04-21 06:46:28 -0700

    linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,24 +7,45 @@
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
  *
- * Return: the address of the new element, or NULL if it fails
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *temp;
 
-	while (str[len])
-		len++;
+	if (head != NULL && str != NULL)
+	{
+		temp = malloc(sizeof(list_t))
+			if (temp == NULL)
+				return (NULL);
 
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (NULL);
+		temp->str = strdup(str);
+		temp->len = _strlen(str);
+		temp->next = *head;
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+		*head = temp;
 
-	return (*head);
+		return (temp);
+	}
+
+	return (0);
+}
+
+/**
+ * _strlen - Returns the lenth of a string
+ * @s: string to count
+ *
+ * Return: string lenth
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+
+	return (c);
 }
