226098d8095b308b3c95f062d7656faa75baea1c
rachelokello <okelloawuor@gmail.com>
2022-06-09 03:48:55 -0700

    doubly linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a new node at the beginning of a doubly linked list
+ * @head: double pointer to the list
+ * @n: data to insert in the new nose
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeoff(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
+	new->next = *head;
+	new->prev = NULL;
+
+	if (*head)
+		(*head)->prev = new;
+
+	*head = new;
+
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+uthis is a readmme for doubly linked list
+the diffrence between it and single linked list is that, the nod3e can be linked forward and backward, prev and next node pointers.
