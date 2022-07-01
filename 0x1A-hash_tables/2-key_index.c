aba2538f4e88059b6aa7978c7f8742104fd73ebb
rachelokello <okelloawuor@gmail.com>
2022-07-01 08:45:40 -0700

    hashtables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - computes the index of a given key in a hash table
+ * @key: the given key
+ * @size: the size of the hash table
+ *
+ * Return: the index of a key in a hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,4 @@
+this readme contains files created on hash tables
+and hash function which is any function that can be used to map data of arbitrary size to fixed size values
+
+the hash functions also contained hash values/hash codes/digests/hashes which are the values returned by a hash function
